653e08a766717ade3311e4542189a1efafe65382
rifalinaaa <99937983+rifalinaaa@users.noreply.github.com>
2022-02-18 10:25:53 +0700

    doraemon

---
+++ Doraemon.java
@@ -0,0 +1,18 @@
+
+package doraemon;
+import java.util.ArrayList;
+public class Doraemon {
+    public static void main(String[] args) {
+        ArrayList kantongAjaib = new ArrayList();
+        
+        kantongAjaib.add("Pintu ke masa depan");
+        kantongAjaib.add(301205);
+        kantongAjaib.add("crush");
+        kantongAjaib.add(123.132);
+        kantongAjaib.add(true);
+        
+        System.out.println(kantongAjaib);
+        System.out.println(kantongAjaib.size());
+    }
+    
+}
